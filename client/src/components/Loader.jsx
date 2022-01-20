6a6c8555177c7efaddda36e20d27d9309c2ab777
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2022-01-20 17:02:55 +0200

    initial commit

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+    return (
+        <div>
+            Loader
+        </div>
+    )
+}
+
+export default Loader
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,35 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
+const Welcome = () => {
+    const connectWallet = () => {
+
+    };
+
+    return (
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto
+                    </p>
+                    <button
+                        type="button"
+                        onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                    >
+                        Connect Wallet
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome;
