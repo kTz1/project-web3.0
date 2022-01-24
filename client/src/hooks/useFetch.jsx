98b675677ce4f63ab8ebdabcb6953a2e334a38b8
kTz1 <catalin_simon90@yahoo.com>
2022-01-24 15:03:39 +0200

    loop the demo transactions, created a new component(TransactionCard) and displaying the demo transactions, created a custom hook to fetch gifs from giphy API

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const API_KEY = import.meta.env.VITER_GIPHY_API;
+
+const useFetch = ({ keyword }) => {
+    const [gifUrl, setGifUrl] = useState("");
+
+    const fetchGifs = async () => {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
+            const { data } =await response.json();
+
+            setGifUrl(data[0]?.images?.downsized_medium?.url);
+        } catch (err) {
+            setGifUrl('https://media4.popsugar-assets.com/files/2013/11/07/832/n/1922398/eb7a69a76543358d_28.gif');
+        }
+    };
+
+    useEffect(() => {
+        if(keyword) fetchGifs();
+    }, [keyword]);
+
+    return gifUrl;
+};
+
+export default useFetch;
